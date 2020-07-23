338e5efba6adb8d32983ac9ea423870ef2726527
changlin <shichanglin@baijiahulian.com>
2020-07-23 19:46:30 +0800

    first

---
+++ hello-gsx/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<!--
+ * @Descripttion:
+ * @version:
+ * @@Company: gsx
+ * @Author: changlin
+ * @Date: 2020-07-23 19:41:26
+ * @LastEditors: changlin
+ * @LastEditTime: 2020-07-23 19:43:15
+-->
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+h3
+  margin 40px 0 0
+
+ul
+  list-style-type none
+  padding 0
+
+li
+  display inline-block
+  margin 0 10px
+
+a
+  color #42b983
+</style>
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @@Company: gsx
+ * @Author: changlin
+ * @Date: 2020-07-23 19:44:58
+ * @LastEditors: changlin
+ * @LastEditTime: 2020-07-23 19:44:58
+ */
